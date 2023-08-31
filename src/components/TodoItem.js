c5b3a3e0a37316eee899013aca67f81e2ff6f836
valentin-stojanov <valentin_stojanov@abv.bg>
2023-08-31 16:00:10 +0300

    Added Strikethrough on the todo item when it is clicked.

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,7 +1,14 @@
-export default function todoItem ({
-    id,
-    text,
-    onDelete
+import styles from './TodoItem.module.css'
+
+export default function todoItem({
+    todo,
+    onDelete,
+    onClick
 }) {
-    return <li>{text} <button onClick={() => onDelete(id)}>x</button></li>
+    return (
+        <li onClick={() => onClick(todo.id)} className={todo.isDone ? styles.todoItemDone : ''}>
+            {todo.text}
+            <button onClick={() => onDelete(todo.id)}>x</button>
+        </li>
+    );
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -4,15 +4,16 @@ import uniqid from 'uniqid';
 
 export default function TodoList() {
     const [todos, setTodos] = useState([
-        { id: 1, text: 'Clean my room' },
-        { id: 2, text: 'Wash the dishes' },
-        { id: 3, text: 'Go to the gym' }
+        { id: 1, text: 'Clean my room', isDone: false },
+        { id: 2, text: 'Wash the dishes', isDone: false },
+        { id: 3, text: 'Go to the gym', isDone: false }
     ]);
 
     const onTodoInputBlur = (e) => {
         let todo = {
             id: uniqid(),
-            text: e.target.value
+            text: e.target.value,
+            isDone: false
         };
 
         setTodos(oldTodos => [...oldTodos, todo]);
@@ -21,14 +22,31 @@ export default function TodoList() {
     }
     const deleteTodoItemClickHandler = (id) => {
         setTodos(oldTodos => oldTodos.filter(todo => todo.id !== id));
-    }
+    };
+
+    const toggleTodoItemClickHandler = (id) => {
+        console.log('Toggle ', id);
+        setTodos(oldTodos => {
+            let selectedTodo = oldTodos.find( x => x.id === id);
+            let toggledTodo = {...selectedTodo, isDone: !selectedTodo.isDone};
+            let restTodos = oldTodos.filter(x => x.id !== id);
+            return [...restTodos, toggledTodo];
+        })
+    };
 
     return (
         <>
             <label htmlFor='todo-name'>Add Todo</label>
             <input type='text' id='todo-name' onBlur={onTodoInputBlur} name='todo'></input>
             <ul>
-                {todos.map(todo => <TodoItem key={todo.id} id={todo.id} text={todo.text} onDelete={deleteTodoItemClickHandler}></TodoItem>)}
+                {todos.map(todo =>
+                    <TodoItem
+                        key={todo.id}
+                        todo={todo}
+                        onDelete={deleteTodoItemClickHandler}
+                        onClick={toggleTodoItemClickHandler}
+                    ></TodoItem>)
+                }
             </ul>
         </>
     );
